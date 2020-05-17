c4c0f0866814095edb1182f01d0bdcada4d229f8
Laura Brown <laura@laurabrown.xyz>
2020-05-17 18:21:52 +0200

    wip responsive images refactor

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('postcss-math'),
     require('tailwindcss'),
     postcssPresetEnv({
       stage: 1
