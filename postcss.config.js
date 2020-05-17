72390f18e06e70d5b86614c8c6d662c1aa63ce95
Laura Brown <laura@laurabrown.xyz>
2020-05-17 19:02:13 +0200

    use postcss-arithmetic

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const postcssPresetEnv = require('postcss-preset-env')
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('postcss-math'),
+    require('postcss-arithmetic'),
     require('tailwindcss'),
     postcssPresetEnv({
       stage: 1
