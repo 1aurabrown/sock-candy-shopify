265f3ee20fe5302f03c43303a4321886aa59003b
Laura Brown <laura@laurabrown.xyz>
2020-05-18 12:42:42 +0200

    style homepage hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,22 +10,22 @@ module.exports = {
       black: '#221F1F',
       'off-white': '#F1EAE4'
     },
-    fontSize: {
+    fontSize: theme => ({
       // mobile styles
-      'base-mob': ['0.875rem'],                       // 14px
-      'lg-mob': ['1.125rem'],                         // 18px
-      'xl-mob': ['1.5rem', '1.6875rem'],              // 24px / 27px
-      '2xl-mob': ['2.0625rem', '2.3125rem'],          // 33px / 37px
+      'base-mob': ['0.875rem', theme('lineHeight.normal')], // 14px
+      'lg-mob': ['1.125rem', theme('lineHeight.normal')],   // 18px
+      'xl-mob': ['1.5rem', '1.6875rem'],                    // 24px / 27px
+      '2xl-mob': ['2.0625rem', '2.3125rem'],                // 33px / 37px
       // desktop styles
-      'sm-desk': ['1.125rem'],                         // 18px
-      'base-desk': ['1.25rem'],                        // 20px
-      'lg-desk': ['1.625rem'],                         // 26px
-      'lg-tight-desk': ['1.625rem', '1.8125rem'],      // 26px / 29px
-      'xl-desk': ['1.75rem'],                          // 28px
-      '2xl-desk': ['3rem'],                            // 48px
-      '2xl-tight-desk': ['3rem', '3.3125rem'],         // 48px / 53px
-      '3xl-desk': ['3.9375rem', '4.375rem']            // 63px / 70px
-    },
+      'sm-desk': ['1.125rem', theme('lineHeight.normal')],  // 18px
+      'base-desk': ['1.25rem', theme('lineHeight.normal')], // 20px
+      'lg-desk': ['1.625rem', theme('lineHeight.normal')],  // 26px
+      'lg-tight-desk': ['1.625rem', '1.8125rem'],           // 26px / 29px
+      'xl-desk': ['1.75rem', theme('lineHeight.normal')],   // 28px
+      '2xl-desk': ['3rem', theme('lineHeight.normal')],     // 48px
+      '2xl-tight-desk': ['3rem', '3.3125rem'],              // 48px / 53px
+      '3xl-tight-desk': ['3.9375rem', '4.375rem']           // 63px / 70px
+    }),
     opacity: {
       '0': '0',
       '10': '.1',
