ebac3c9b7f0204a33c0678ddcd1f8ed0015bda42
Laura Brown <laura@laurabrown.xyz>
2020-05-21 19:00:19 +0200

    refine footer padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,19 +17,20 @@ module.exports = {
     },
     fontSize: theme => ({
       // mobile styles
-      'base-mob': ['0.875rem', theme('lineHeight.normal')], // 14px
-      'lg-mob': ['1.125rem', theme('lineHeight.normal')],   // 18px
-      'xl-mob': ['1.5rem', '1.6875rem'],                    // 24px / 27px
-      '2xl-mob': ['2.0625rem', '2.3125rem'],                // 33px / 37px
+      'base-mob': ['0.875rem', theme('lineHeight.normal')],            // 14px
+      'lg-mob': ['1.125rem', theme('lineHeight.normal')],              // 18px
+      'xl-mob': ['1.5rem', theme('lineHeight.tight-mob')],             // 24px
+      '2xl-mob': ['2.0625rem', theme('lineHeight.tight-mob')],         // 33px
       // desktop styles
-      'sm-desk': ['1.125rem', theme('lineHeight.normal')],  // 18px
-      'base-desk': ['1.25rem', theme('lineHeight.normal')], // 20px
-      'lg-desk': ['1.625rem', theme('lineHeight.normal')],  // 26px
-      'lg-tight-desk': ['1.625rem', '1.8125rem'],           // 26px / 29px
-      'xl-desk': ['1.75rem', theme('lineHeight.normal')],   // 28px
-      '2xl-desk': ['3rem', theme('lineHeight.normal')],     // 48px
-      '2xl-tight-desk': ['3rem', '3.3125rem'],              // 48px / 53px
-      '3xl-tight-desk': ['3.9375rem', '4.375rem']           // 63px / 70px
+      'sm-desk': ['1.125rem', theme('lineHeight.normal')],             // 18px
+      'base-desk': ['1.25rem', theme('lineHeight.normal')],            // 20px
+      'lg-desk': ['1.625rem', theme('lineHeight.normal')],             // 26px
+      'lg-tight-desk': ['1.625rem', theme('lineHeight.tight-desk')],   // 26px
+      'xl-desk': ['1.75rem', theme('lineHeight.normal')],              // 28px
+      'xl-tight-desk': ['1.75rem', theme('lineHeight.tight-desk')],    // 28px
+      '2xl-desk': ['3rem', theme('lineHeight.normal')],                // 48px
+      '2xl-tight-desk': ['3rem', theme('lineHeight.tight-desk')],      // 48px
+      '3xl-tight-desk': ['3.9375rem', theme('lineHeight.tight-desk')]  // 63px
     }),
     borderWidth: {
       default: '2px',
@@ -49,7 +50,9 @@ module.exports = {
       normal: 'auto',
     },
     lineHeight: {
-      'normal': '1.3'
+      'normal': '1.3',
+      'tight-mob': '1.12',
+      'tight-desk': '1.11'
     },
     extend: {
       opacity: {
