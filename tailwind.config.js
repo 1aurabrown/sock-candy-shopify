bb3a767924c2b4aec1d05aa249cbf4b088375a29
Laura Brown <laura@laurabrown.xyz>
2020-05-23 13:10:34 +0200

    add related products section, fix spacing on pdp split view

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,20 @@ module.exports = {
         tint: theme('colors.black.tint', 'currentColor'),
       }
     }),
+    zIndex: {
+      '0': 0,
+      '10': 10,
+      '20': 20,
+      '30': 30,
+      '40': 40,
+      '50': 50,
+      '60': 60,
+      '70': 70,
+      '80': 80,
+      '90': 90,
+      '100': 100,
+      'auto': 'auto',
+    },
     letterSpacing: {
       normal: 'auto',
     },
