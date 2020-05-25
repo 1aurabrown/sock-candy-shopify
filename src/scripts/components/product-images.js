bb36882c07603074f755fb22d67b694f1776ed4b
Laura Brown <laura@laurabrown.xyz>
2020-05-25 15:02:49 +0200

    abstract pdp image slider to class

---
+++ src/scripts/components/product-images.js
@@ -0,0 +1,50 @@
+import Swiper from 'swiper';
+
+const selectors = {
+  swiperContainer: '.swiper-container',
+  slides: '.swiper-slide',
+  prev: '.swiper-button-prev',
+  next:'.swiper-button-next',
+  pagination:'.pdp-images__pagination',
+  thumbs: '.pdp-images__thumb'
+}
+
+export default class ProductImages {
+  constructor({container}) {
+    this.container = container
+    this.thumbClicked = this.thumbClicked.bind(this)
+    const imagesEl = this.container.querySelector(selectors.swiperContainer)
+    this.thumbs = this.container.querySelectorAll(selectors.thumbs)
+    this.thumbs.forEach((thumb) => {
+      thumb.addEventListener('click', this.thumbClicked)
+    })
+
+    this.imageSwiper = new Swiper(imagesEl, {
+      loop: true,
+      navigation: {
+        nextEl: selectors.next,
+        prevEl: selectors.prev,
+      },
+
+      pagination: {
+        el: selectors.pagination,
+        type: 'bullets',
+      },
+
+      slidesPerView: 1,
+      centeredSlides: true
+    });
+  }
+
+  thumbClicked(e) {
+    const index = parseInt(e.currentTarget.dataset.index)
+    this.imageSwiper.slideToLoop(index)
+  }
+
+  destroy() {
+    if (this.imageSwiper) this.imageSwiper.destroy();
+    this.thumbs.forEach(thumb => {
+      thumb.removeEventListener('click', this.thumbClicked)
+    })
+  }
+}
--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -1,52 +1,16 @@
 import {register} from '@shopify/theme-sections'
-import Swiper from 'swiper';
-import { screens } from '../core/breakpoints-tailwind.js';
+import ProductImages from '../components/product-images.js';
 
-const selectors = {
-  swiperContainer: '.pdp-images .swiper-container',
-  slides: '.pdp-images .swiper-slide',
-  prev: '.pdp-images .swiper-button-prev',
-  next:'.pdp-images .swiper-button-next',
-  pagination:'.pdp-images__pagination',
-  thumbs: '.pdp-images__thumb'
-}
 
 register('product', {
   onLoad() {
-    this.thumbClicked = this.thumbClicked.bind(this)
-    const imagesEl = this.container.querySelector(selectors.swiperContainer)
-    this.thumbs = this.container.querySelectorAll(selectors.thumbs)
-    this.thumbs.forEach((thumb) => {
-      thumb.addEventListener('click', this.thumbClicked)
+    this.productImages = new ProductImages({
+      container: this.container.querySelector('.pdp-images')
     })
-
-    this.imageSwiper = new Swiper(imagesEl, {
-      loop: true,
-      navigation: {
-        nextEl: selectors.next,
-        prevEl: selectors.prev,
-      },
-
-      pagination: {
-        el: selectors.pagination,
-        type: 'bullets',
-      },
-
-      slidesPerView: 1,
-      centeredSlides: true
-    });
-  },
-
-  thumbClicked(e) {
-    const index = parseInt(e.currentTarget.dataset.index)
-    this.imageSwiper.slideToLoop(index)
   },
 
   onUnload() {
-    if (this.imageSwiper) this.imageSwiper.destroy();
-    this.thumbs.forEach(thumb => {
-      thumb.removeEventListener('click', this.thumbClicked)
-    })
+    this.productImages.destroy()
   }
 });
 
