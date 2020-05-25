6637e5480420c0b9f95553579669bfbeef7aba81
Laura Brown <laura@laurabrown.xyz>
2020-05-25 15:54:04 +0200

    product details toggles

--- src/scripts/sections/product.js
+++ src/scripts/sections/product.js
@@ -1,16 +1,20 @@
 import {register} from '@shopify/theme-sections'
 import ProductImages from '../components/product-images.js';
-
+import ProductDetails from '../components/product-details.js';
 
 register('product', {
   onLoad() {
     this.productImages = new ProductImages({
       container: this.container.querySelector('.pdp-images')
     })
+    this.details = new ProductDetails({
+      container: this.container.querySelector('.pdp-details')
+    })
   },
 
   onUnload() {
     this.productImages.destroy()
+    this.details.destroy()
   }
 });
 
