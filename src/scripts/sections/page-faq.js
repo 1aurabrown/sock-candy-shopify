44ed7c6eca4937aefeb4c4415c5b2f5df3d1b474
Laura Brown <laura@laurabrown.xyz>
2020-06-03 13:11:00 +0200

    faq slide toggle

--- src/scripts/sections/page-faq.js
+++ src/scripts/sections/page-faq.js
@@ -1,16 +1,39 @@
+import 'dom-slider'
 import {register} from '@shopify/theme-sections'
 
 const selectors = {
-
+  itemContainer: '.faq-item',
+  itemHeading: '.faq-item__heading',
+  itemContent: '.faq-item__content'
 }
 
 register('page-faq', {
   onLoad() {
+    this.headingClicked = this.headingClicked.bind(this)
+    this.headings = this.container.querySelectorAll(selectors.itemHeading)
+    this.contents = this.container.querySelectorAll(selectors.itemContent)
+    this.headings.forEach((heading) => {
+      heading.addEventListener('click', this.headingClicked)
+    })
 
   },
 
-  onUnload() {
+  headingClicked(e) {
+    const heading = e.currentTarget
+    const thisContent = heading.parentNode.querySelector(selectors.itemContent)
+    Array.from(this.contents).forEach(content => {
+      if (content === thisContent) return
+      domSlider.slideUp({ element: content })
+    })
 
+    domSlider.slideToggle({ element: thisContent})
+
+  },
+
+  onOnload() {
+    this.headings.forEach(heading => {
+      heading.removeEventListener('click', this.headingClicked)
+    })
   }
 });
 
